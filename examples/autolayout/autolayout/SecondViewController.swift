54d3d0155d1c67c4e92b984d89ed773b82733b8f
Eric Reid <reidek@gmail.com>
2015-10-20 19:47:28 -0600

    Add autolayout example

---
+++ examples/autolayout/autolayout/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  autolayout
+//
+//  Created by Eric Reid on 10/20/15.
+//  Copyright © 2015 Eric Reid. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
